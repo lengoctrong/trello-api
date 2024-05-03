d3e72759a99d5bc46cfe47230ab65b1c5c27fc7e
Trong Le <asurakul205@gmail.com>
2024-05-03 22:48:10 +0700

    update code

--- src/services/userService.js
+++ src/services/userService.js
@@ -13,15 +13,19 @@ const create = async (data) => {
       role: 'user'
     }
 
-    if (type === 'login') {
-      const existingUser = await userModel.findUser({ email, password })
-      if (!existingUser) {
-        throw new ApiError(StatusCodes.NOT_FOUND, 'User not found')
-      }
-      return existingUser
+    const existingUser = await userModel.findUser({ email })
+
+    if (type === 'login' && !existingUser) {
+      throw new ApiError(StatusCodes.NOT_FOUND, 'Không tìm thấy người dùng!')
     }
 
-    if (type === 'register') {
+    if (type === 'login' && existingUser) return existingUser
+
+    if (type === 'register' && existingUser) {
+      throw new ApiError(StatusCodes.CONFLICT, 'Người dùng đã tồn tại!')
+    }
+
+    if (type === 'register' && !existingUser) {
       const result = await userModel.create(userData)
       return await userModel.findOneById(result.insertedId)
     }
